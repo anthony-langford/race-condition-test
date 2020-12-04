4eb2d68b19c702bca977877a557feae227ae4b9c
Anthony Langford <anthony.langford@gmail.com>
2020-12-04 10:41:59 -0500

    update path and timeout

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:3000";
-axios.defaults.timeout = 1000;
+axios.defaults.timeout = 5000;
 axios.defaults.headers.common = {
   Authorization:
-    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IndhcmVob3VzZWd1eUBzZWNvbmRjbG9zZXQuY29tIiwiaWF0IjoxNjA2NzUzNDg0LCJleHAiOjE2MDY3ODk0ODR9.0IoLvcSzYm148HFhSqhObxNktCL3J8_eonkyAbz-8mg",
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IndhcmVob3VzZWd1eUBzZWNvbmRjbG9zZXQuY29tIiwiaWF0IjoxNjA3MDMxMjIzLCJleHAiOjE2MDcwNjcyMjN9.D3q76RCqIG-8HitPomU2vJuOLBpEze5A1UsaiB9Y_Gs",
   "Content-Type": "application/json; charset=utf-8",
 };
 
@@ -17,7 +17,7 @@ const updateLocationItem = () => {
 };
 
 const updateLocationItem2 = () => {
-  return axios.post("location-items/move-items-to-location", {
+  return axios.post("/location-items/move-items-to-location", {
     items: [{ sku: "123", quantity: 1 }],
     from_location_code: "5A-2-4-8",
     to_location_code: "5A-1-2-3",
@@ -30,7 +30,7 @@ const run = async () => {
       updateLocationItem(),
       updateLocationItem2(),
     ]);
-    console.log(results[0].data, results[1].data);
+    console.log(results[0]?.data, results[1]?.data);
   } catch (error) {
     console.error(error.message);
   }
