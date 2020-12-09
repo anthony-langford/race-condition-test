48a664a71fe17c5a978716ebeab56c0fe563c26e
Anthony Langford <anthony.langford@gmail.com>
2020-12-09 13:14:46 -0500

    🎉

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,21 @@ axios.defaults.baseURL = "http://localhost:3000";
 axios.defaults.timeout = 5000;
 axios.defaults.headers.common = {
   Authorization:
-    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IndhcmVob3VzZWd1eUBzZWNvbmRjbG9zZXQuY29tIiwiaWF0IjoxNjA3MDMxMjIzLCJleHAiOjE2MDcwNjcyMjN9.D3q76RCqIG-8HitPomU2vJuOLBpEze5A1UsaiB9Y_Gs",
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IndhcmVob3VzZWd1eUBzZWNvbmRjbG9zZXQuY29tIiwiaWF0IjoxNjA3NDQ0MDc2LCJleHAiOjE2MDc0ODAwNzZ9.elfG5xFjfCAQa7WYCFUiYBdlIyLUkuGfwOlXcZFLBWE",
   "Content-Type": "application/json; charset=utf-8",
 };
 
 const updateLocationItem = () => {
-  return axios.post("/location-items/move-items-to-location", {
-    items: [{ sku: "123", quantity: 1 }],
-    from_location_code: "5A-2-4-8",
-    to_location_code: "5A-1-2-3",
+  return axios.post("/location-items/move-items-to-pallet", {
+    items: [{ sku: "meatball", quantity: 3 }],
+    pallet_id: "PL-aOaJ4hb2",
   });
 };
 
 const updateLocationItem2 = () => {
-  return axios.post("/location-items/move-items-to-location", {
-    items: [{ sku: "123", quantity: 1 }],
-    from_location_code: "5A-2-4-8",
-    to_location_code: "5A-1-2-3",
+  return axios.post("/location-items/move-items-to-pallet", {
+    items: [{ sku: "meatball", quantity: 3 }],
+    pallet_id: "PL-aOaJ4hb2",
   });
 };
 
@@ -32,7 +30,10 @@ const run = async () => {
     ]);
     console.log(results[0]?.data, results[1]?.data);
   } catch (error) {
-    console.error(error.message);
+    console.log(error.message);
+    console.error(
+      `${error?.response?.data?.statusCode} ${error?.response?.data?.message}`
+    );
   }
 };
 
